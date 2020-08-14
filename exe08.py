98b890479bb073145067fab6828993f24d1f44dc
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-14 18:29:12 -0300

    Add files via upload

---
+++ exe08.py
@@ -0,0 +1,8 @@
+# Leia uma temperatura em graus Kalvim e apresente-a convertida em graus Celsius.
+# A formula de conversão é: C = K - 273.15, sendo C a temperatura em Celsius e K
+# a temperatura em Kelvin.
+
+k = 300
+c = k - 273.15
+print(c)
+
