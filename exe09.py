af47a7c204a7b9ee5ab04773ecf2ad0cd23d2421
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-14 18:29:53 -0300

    Add files via upload

---
+++ exe09.py
@@ -0,0 +1,7 @@
+# Leia uma temperatura em graus Celsius e apresente-a convertida em graus Kelvin.
+# A formula de conversão é: K = C + 273.15, sendo C a temperatura em Celsius e
+# K a temperatura em Kelvin
+
+c = 20
+k = c + 273.15
+print(k)
