329520609394125469a07a5ecfffd862b685781c
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-14 18:27:18 -0300

    Add files via upload

---
+++ exe06.py
@@ -0,0 +1,7 @@
+# Leia uma tempperatura em graus Celsius e apresente-a convertida em graus Fahrenhit.
+# A formula de conversão é : F = C * (9.0/5.0) + 32.0, sendo F a temperatura em Fahrenhit e
+# C a temperatura em Celsius.
+
+c = 21
+f = c * (9.0 / 5.0) + 32.0
+print(f)
