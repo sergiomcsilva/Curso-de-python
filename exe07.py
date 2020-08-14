1c5a0696b3ff8cfa0b04476e69fa5f96ef3ced34
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-14 18:28:02 -0300

    Add files via upload

---
+++ exe07.py
@@ -0,0 +1,8 @@
+# Leia uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
+# A formula de converção é C = 5.0 * (F - 32.0) / 9.0. sendo C a temperatura em Celsius e
+# F a temperatura em Fahrenheit
+
+f = 25
+c = 5.0 * (f - 32.0) / 9.0
+print(c)
+
