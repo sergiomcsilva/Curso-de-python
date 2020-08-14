d1de8e9f57adf989dc4351e04f28c609642a2ab9
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-14 18:25:42 -0300

    Add files via upload

---
+++ exe03.py
@@ -0,0 +1,8 @@
+# Peça ao usuario para digitar tres valores inteiro e imprima a soma deles
+
+num1 = int(input('digite o numero1: '))
+num2 = int(input('digite o numero2: '))
+num3 = int(input('digite o numero3: '))
+resultado = num1 + num2 + num3
+print(f'A soma de {num1}, {num2}, {num3} = ', resultado)
+
