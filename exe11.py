2aed0a13fb5e1b8ac66f91b7a509b98a1c93696d
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:11:36 -0300

    Add files via upload

---
+++ exe11.py
@@ -0,0 +1,8 @@
+# Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em km/h
+# (quilometros por hora). A fórmula de conversão é : K = M * 3.6. sendo K a velocidade
+# em km/h e M em m/s.
+
+ms = 15
+k = ms * 3.6
+print(k)
+
