c2b20ccd5371a78aae85a0bcf1a2807515885217
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:14:52 -0300

    Add files via upload

---
+++ exe18.py
@@ -0,0 +1,11 @@
+# Leia um valor de volume em metros cubicos m³ e apresente-o convertido em litros. A
+# fõrmula de covnersão é: L = 100 * M, sendo L o volume em litros e M o volume em metros
+# cúbicos.
+
+m = 10
+l = 1
+m = 2
+
+l = 100 * m
+
+print(l)
