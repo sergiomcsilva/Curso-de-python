5b251c8fab61c9b281191a64a926364897b104ee
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:13:15 -0300

    Add files via upload

---
+++ exe15.py
@@ -0,0 +1,9 @@
+# Leia um ângulo em radianos e apresente-o convertido em graus. A fòrmula de conversão
+# e: G = R * 180 / π, sendo G o ângulo em graus e R em radianos e π = 3.14.
+
+r = 2
+g = 1
+π = 3.14
+g = r * 180 / π
+print(g)
+
