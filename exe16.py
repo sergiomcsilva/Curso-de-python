aa662bd3beb1234ad0a32282f5daa793cf52ae81
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:13:37 -0300

    Add files via upload

---
+++ exe16.py
@@ -0,0 +1,9 @@
+# Leia um valor de comprimento em polegadas e apresente-o convertido em centimetros.
+# A fórmula de conversão é: C = P * 2.54, sendo C o comprimento em centimetros e P o
+# comprimento em polegadas.
+
+c = 25
+p = 5
+c = p * 2.54
+print(c)
+
