082577521ecb9b151e68429e7ded01b90c336e72
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:12:02 -0300

    Add files via upload

---
+++ exe12.py
@@ -0,0 +1,7 @@
+# Leia uma distância em milhas e apresente-a convertida em quilometros. A fórmula de
+# conversão é: K = 1.61 * M, sendo K a distancia em quilometros em M em milhas.
+
+m = 14
+k = 1.61 * m
+print(k)
+
