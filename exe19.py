5d8d81b3bd61fbd09cba4da0ceb9fbe5e8b3014a
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:15:40 -0300

    Add files via upload

---
+++ exe19.py
@@ -0,0 +1,10 @@
+# Leia um valor de volume em litros e apresente-o convertido em metros cúbicos m³. A
+# fórmula de conversão é: M = L / 1000, sendo L, o volume e o M o volume em metros
+# cúbicos.
+
+l = 3.6
+m = 2.0
+
+m = l / 1000
+print(m)
+
