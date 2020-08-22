04647ea9244fb63c1a5fe20239b6de2b088ba928
sergiomcsilva <sergiomcsilva@yahoo.com.br>
2020-08-22 13:16:02 -0300

    Add files via upload

---
+++ exe20.py
@@ -0,0 +1,8 @@
+# Leia um valor em massa em quilogramas e apresente-o convertido em libras. A fórmula
+# de conversão é: L = M / 0.45, sendo K a massa em quilogramas e L a massa em libras.
+
+l = 2
+m = 4
+l = m / 0.45
+print(l)
+
